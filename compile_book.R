5ef6b7504d5ccbb74e3af7a2798d1cfa33494840
Eric Leung <eric@erictleung.com>
2019-04-19 12:35:31 -0700

    Comment and add shebang to compile script
    Just to make it more explicit on how to run script.

--- compile_book.R
+++ compile_book.R
@@ -1,1 +1,7 @@
+#!/usr/bin/env Rscript
+
+# Purpose: Compile contents of book into ./docs/ directory
+# Usage: Rscript compile_book.R
+
+# Render book into docs/ directory
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
